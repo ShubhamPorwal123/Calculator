8f1b1b198e4560c2f3f1d80e0c6a910f2908f053
ShubhamPorwal123 <75979160+ShubhamPorwal123@users.noreply.github.com>
2022-07-20 19:34:31 +0530

    Adding sum functionality to Calc.java

---
+++ Calc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Calc
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    int numOne = scan.nextInt();
+    int numTwo = scan.nextInt();
+    
+    System.out.println("The sum of given number is : " + (numOne + numTwo));
+  }
+}
